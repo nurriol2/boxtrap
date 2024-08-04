ea1bdbdbba35b36a88991d8378799ac6ddb6678b
nurriol2 <niko.urriola1@gmail.com>
2024-08-03 19:18:38 -0500

    Add static scene with camera

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,58 @@
+#![warn(clippy::all, clippy::pedantic)]
+
 use bevy::prelude::*;
 
 // Resources
-#[derive(Resource)]
-struct GreetTimer(Timer);
 
 // Components
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
 
 // Systems
-fn hello_world_system() {
-    // Simple system
-    println!("hello world!");
-}
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Alice".to_string())));
-    commands.spawn((Person, Name("Bob".to_string())));
-    commands.spawn((Person, Name("Charlie".to_string())));
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Spawn shapes
+    // Circular plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Circle::new(4.0)),
+        material: materials.add(Color::WHITE),
+        transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+        ..default()
+    });
+
+    // Cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+        material: materials.add(Color::srgb_u8(124, 144, 255)),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+
+    // Light the scene
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
 
 // Queries
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    // Update the timer with the time elapsed since last update
-    // If the update caused the timer to finish, print
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("hello {}!", name.0);
-        }
-    }
-}
-
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Alice" {
-            name.0 = "Alice L.".to_string();
-            break;
-        }
-    }
-}
 
 // Plugins
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
-        app.add_systems(Startup, add_people); // Register system on the Startup scheduler
-        app.add_systems(
-            // Systems run in parallel
-            Update,
-            (
-                //hello_world_system,
-                (update_people, greet_people).chain(), // Chain specifies the order
-            ),
-        );
-    }
-}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(HelloPlugin)
+        .add_systems(Startup, setup)
         .run();
 }
